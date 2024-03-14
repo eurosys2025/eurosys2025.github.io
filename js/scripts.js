6395584bb27ea9c1219794bca2fbcfc0776740cd
Georgios Smaragdakis <g.smaragdakis@tudelft.nl>
2024-03-15 00:48:17 +0100

    10imgs

--- js/scripts.js
+++ js/scripts.js
@@ -49,7 +49,8 @@ $(document).ready(function(){
         'img/img06.jpg',
         'img/img07.jpg',
         'img/img08.jpg',
-		'img/img09.jpg'];
+		'img/img09.jpg',
+		'img/img10.jpg'];
 
     var randomNumber = Math.floor(Math.random() * images.length);
     var bgImg = 'url(' + images[randomNumber] + ')';
